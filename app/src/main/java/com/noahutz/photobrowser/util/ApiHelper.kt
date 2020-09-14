b40f35a4779075f336741695f243e333b1477ecb
Noah Utzurrum <noah.utzurrum@gmail.com>
2020-09-14 23:35:01 +1000

    Added sort by title to Album List in ViewModel

--- app/src/main/java/com/noahutz/photobrowser/util/ApiHelper.kt
+++ app/src/main/java/com/noahutz/photobrowser/util/ApiHelper.kt
@@ -1,7 +1,6 @@
 package com.noahutz.photobrowser.util
 
 import retrofit2.Response
-import java.lang.Exception
 
 object ApiHelper {
     suspend fun <T> getResult(call: suspend () -> Response<T>): ResultOf<T> {
@@ -15,6 +14,7 @@ object ApiHelper {
         } catch (e: Exception) {
             return ResultOf.Failure(e.message ?: "Error getResult to function $call", e)
         }
-        return ResultOf.Failure("Error getResult to function $call")
+        val message = "Error getResult to function $call"
+        return ResultOf.Failure(message, Exception(message))
     }
 }
--- app/src/main/java/com/noahutz/photobrowser/viewmodel/AlbumListViewModel.kt
+++ app/src/main/java/com/noahutz/photobrowser/viewmodel/AlbumListViewModel.kt
@@ -2,13 +2,18 @@ package com.noahutz.photobrowser.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.noahutz.photobrowser.model.Album
 import com.noahutz.photobrowser.repository.AlbumRepository
 import com.noahutz.photobrowser.util.ResultOf
+import com.noahutz.photobrowser.util.map
 
 class AlbumListViewModel @ViewModelInject constructor(
     private val repository: AlbumRepository
 ) : ViewModel() {
-    fun getAlbums(): LiveData<ResultOf<List<Album>>> = repository.getAlbums()
+    fun getAlbums(): LiveData<ResultOf<List<Album>>> =
+        Transformations.map(repository.getAlbums()) { result ->
+            result.map { albums -> albums.sortedBy { it.title } }
+        }
 }
